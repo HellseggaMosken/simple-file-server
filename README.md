062b1f7bed907fe4246a90917a52ee013731b71a
HellseggaMosken <thetemp@qq.com>
2020-08-15 19:14:43 +0800

    Sat 15 Aug 2020 07:14:43 PM CST

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple File Server
+
+A simple file server for development, temporary file sharing...
+
+## Usage
+
+options:
+
+`help`/`h` : show usage  
+`port`/`p` + port : set server port
+
+default port is 8080
+
+## Build
+
+```shell
+# git clone xxx your-folder
+cd your-folder
+go build
+```
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 // 获取本机网卡IPv4地址
@@ -25,19 +26,42 @@ func getLocalIPv4() (string, error) {
 			}
 		}
 	}
-
 	return "", errors.New("can't get local ipv4 ip")
 }
 
+const usage = `ACCEPT OPTIONS:
+h/help : show this usage
+p/port [port number] : set server port, default 8080`
+
 func main() {
+	port := "8080"
+
+	if len(os.Args) > 1 {
+		op := os.Args[1]
+		if op == "help" || op == "h" {
+			fmt.Println(usage)
+			return
+		} else if op == "port" || op == "p" {
+			if len(os.Args) > 2 {
+				if _, err := strconv.Atoi(os.Args[2]); err == nil {
+					port = os.Args[2]
+				} else {
+					fmt.Println("port should be a number, but got \"" + os.Args[2] + "\"")
+					return
+				}
+			}
+		} else {
+			fmt.Println("unknown option: " + op + ", use option \"help\" for help")
+			return
+		}
+	}
+
 	dir, err := os.Getwd()
 	if err != nil {
 		panic("can't get the current working dir's path")
 	}
 
-	port := "8080"
-
-	fmt.Println("server at:")
+	fmt.Println("serving \"" + dir + "\" at:")
 	fmt.Println("http://localhost:" + port)
 	if ipv4, err := getLocalIPv4(); err == nil {
 		fmt.Println("http://" + ipv4 + ":" + port)
@@ -46,5 +70,7 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(dir)))
-	http.ListenAndServe(":"+port, nil)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		panic(err)
+	}
 }
