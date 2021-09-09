98df72ff876875b1acd078b23004a39fc0855088
HellseggaMosken <thetemp@qq.com>
2021-09-09 18:08:07 +0800

    09/09/2021 18:08:07

--- main.go
+++ main.go
@@ -9,23 +9,23 @@ import (
 	"strconv"
 )
 
-// 获取本机网卡IPv4地址
 func getLocalIPv4() (string, error) {
-	// 获取所有网卡
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		return "", err
 	}
-	// 取第一个非lo的网卡IP
+
 	for _, addr := range addrs {
-		// 这个网络地址是IP地址: ipv4, ipv6
-		if ipNet, isIPNet := addr.(*net.IPNet); isIPNet && !ipNet.IP.IsLoopback() {
-			// 跳过IPV6
-			if ipNet.IP.To4() != nil {
-				return ipNet.IP.String(), nil
-			}
+		ipNet, isIPNet := addr.(*net.IPNet)
+		if !isIPNet {
+			continue
+		}
+
+		if !ipNet.IP.IsLoopback() && ipNet.IP.IsGlobalUnicast() && ipNet.IP.To4() != nil {
+			return ipNet.IP.String(), nil
 		}
 	}
+
 	return "", errors.New("can't get local ipv4 ip")
 }
 
